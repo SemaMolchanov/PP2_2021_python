0def358c40ea551a0a593fa9855ca6fb9320fd4a
SemaMolchanov <sema.molchanov@gmail.com>
2021-02-09 13:01:55 +0600

    w3school updated

--- w3school/q.py
+++ w3school/q.py
@@ -1,7 +1,7 @@
 # We can combine strings and numbers by using the format() method!
 # The format() method takes the passed arguments, formats them, and places them in the string where the placeholders {} are:
 
-age = 36
+age = 45
 txt = "My name is John, and I am {}"
 print(txt.format(age))
 
