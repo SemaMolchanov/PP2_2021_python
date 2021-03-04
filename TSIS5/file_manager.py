2da48a96e93595887b3a20caeae91566f63bf4ec
SemaMolchanov <sema.molchanov@gmail.com>
2021-03-04 21:46:10 +0600

    TSIS5 updated

--- TSIS5/file_manager.py
+++ TSIS5/file_manager.py
@@ -26,61 +26,94 @@ def return_to_parent_dir():
 
 def change_directory():
     global cur_location
-    print('''
-    press 1 to return to the parent directory
-    press 2 to change directory''')
-    option = int(input())
-    if option == 1:
-        return_to_parent_dir()
-    elif option == 2:
-        new_location = input()
-        if os.path.exists(os.path.join(cur_location, new_location)):
-            os.chdir(os.path.join(cur_location, new_location))
-            cur_location = os.path.join(cur_location, new_location)
-            print ('your current location is', cur_location)
-            print('do you want to continue? (yes/no)')
-            option = input()
-            if option == 'yes':
-                start()
-            elif option == 'no':
-                print('Thank you. Good bue')
-        else:
-            print('this folder does not exist')
-            print('do you want to continue? (yes/no)')
-            option = input()
-            if option == 'yes':
-                start()
-            elif option == 'no':
-                print('Thank you. Good bue')
+    print('enter the name of directory you want to move to')
+    new_location = input()
+    if os.path.exists(os.path.join(cur_location, new_location)):
+        os.chdir(os.path.join(cur_location, new_location))
+        cur_location = os.path.join(cur_location, new_location)
+        print ('your current location is', cur_location)
+        print('do you want to continue? (yes/no)')
+        option = input()
+        if option == 'yes':
+            start()
+        elif option == 'no':
+            print('Thank you. Good bue')
+    else:
+        print('this folder does not exist')
+        print('do you want to continue? (yes/no)')
+        option = input()
+        if option == 'yes':
+            start()
+        elif option == 'no':
+            print('Thank you. Good bue')
 
 def rename_directory():
-    print('enter the name you want the directory to be changed to')
-    old_name = cur_location
-    new_name = input()
-    os.rename(old_name, new_name)
-    pass
+    global cur_location
+    print('enter the name of the directory you want to rename')
+    dir_name = input()
+    if os.path.exists(os.path.join(cur_location, dir_name)):
+        print('enter the new name')
+        new_name = input()
+        os.rename(os.path.join(cur_location, dir_name), os.path.join(cur_location, new_name))
+        print('directory succesfuly renamed')
+        list_content()
+        print('such a directory does not exist')
+        print('do you want to continue? (yes/no)')
+        option = input()
+        if option == 'yes':
+            start()
+        elif option == 'no':
+            print('Thank you. Good bue')
+    else:
+        print('such a directory does not exist')
+        print('do you want to continue? (yes/no)')
+        option = input()
+        if option == 'yes':
+            start()
+        elif option == 'no':
+            print('Thank you. Good bue')
 
 def print_files_number():
     files_number = len([name for name in os.listdir(cur_location) if os.path.isfile(os.path.join(cur_location, name))])
-    print(files_number)
+    print(str(files_number) + ' in the current directory')
+    print('do you want to continue? (yes/no)')
+    option = input()
+    if option == 'yes':
+        start()
+    elif option == 'no':
+        print('Thank you. Good bue')
+
     pass
 
 def print_dirs_number():
     dirs_number = len([name for name in os.listdir(cur_location) if os.path.isdir(os.path.join(cur_location, name))])
-    print(dirs_number)
+    print(str(dirs_number) + ' directories in the current directory')
+    print('do you want to continue? (yes/no)')
+    option = input()
+    if option == 'yes':
+        start()
+    elif option == 'no':
+        print('Thank you. Good bue')
     pass
 
 def list_content():
+    print('all directories and files of the current directory')
     dir_list = os.listdir(cur_location)
     for directory in dir_list:
         print(directory)
+    print('do you want to continue? (yes/no)')
+    option = input()
+    if option == 'yes':
+        start()
+    elif option == 'no':
+        print('Thank you. Good bue')
     pass
 
 def add_file():
     print('''
     enter the name of the file you want to create in the following format
 
-    file_name.extension''')
+                        file_name.extension''')
     default_data = str()
     global cur_location
     file_name = input()
@@ -109,6 +142,13 @@ def add_dir():
     dir_name = input()
     if not os.path.exists(os.path.join(cur_location, dir_name)):
         os.mkdir(os.path.join(cur_location, dir_name))
+        print('''directory has been created''')
+        print('do you want to continue? (yes/no)')
+        option = input()
+        if option == 'yes':
+            start()
+        elif option == 'no':
+            print('Thank you. Good bue')
     else:
         print('such a directory already exists')
         print('do you want to continue? (yes/no)')
@@ -117,71 +157,153 @@ def add_dir():
             start()
         elif option == 'no':
             print('Thank you. Good bue')
-    pass
 
 def delete_file():
-    pass
+    print('''enter name of the file you want to delete in the following format
+    
+                        file_name.extension''')
+    file_name = input()
+    if os.path.exists(os.path.join(cur_location, file_name)):
+        os.remove(os.path.join(cur_location, file_name))
+        print('file removed succesfully')
+        print('do you want to continue? (yes/no)')
+        option = input()
+        if option == 'yes':
+            start()
+        elif option == 'no':
+            print('Thank you. Good bue')
+    else:
+        print('such a file does not exist')
+        print('do you want to continue? (yes/no)')
+        option = input()
+        if option == 'yes':
+            start()
+        elif option == 'no':
+            print('Thank you. Good bue')
 
 def rename_file():
-    pass
+    print('''enter name of the file you want to rename in the following format
+    
+                        file_name.extension''')
+    file_name = input()
+    if os.path.exists(os.path.join(cur_location, file_name)):
+        print('enter the new name')
+        new_name = input()
+        os.rename(os.path.join(cur_location, file_name), os.path.join(cur_location, new_name))
+        print('file renamed succesfully')
+        print('do you want to continue? (yes/no)')
+        option = input()
+        if option == 'yes':
+            start()
+        elif option == 'no':
+            print('Thank you. Good bue')
+    else:
+        print('such a file does not exist')
+        print('do you want to continue? (yes/no)')
+        option = input()
+        if option == 'yes':
+            start()
+        elif option == 'no':
+            print('Thank you. Good bue')
 
 def add_content():
-    pass
+    print('''enter name of the file you want to add content to in the following format
+    
+                        file_name.extension''')
+    file_name = input()
+    if os.path.exists(os.path.join(cur_location, file_name)):
+        with open (os.path.join(cur_location, file_name), 'a') as file:
+            print('enter data to write to the file')
+            data = input()
+            file.write(data)
+            file.close
+        print('content added succesfully')
+        print('do you want to continue? (yes/no)')
+        option = input()
+        if option == 'yes':
+            start()
+        elif option == 'no':
+            print('Thank you. Good bue')
+    else:
+        print('such a file does not exist')
+        print('do you want to continue? (yes/no)')
+        option = input()
+        if option == 'yes':
+            start()
+        elif option == 'no':
+            print('Thank you. Good bue')
 
 def rewrite_content():
+    print('''enter name of the file you want to add content to in the following format
+    
+                        file_name.extension''')
+    file_name = input()
+    if os.path.exists(os.path.join(cur_location, file_name)):
+        with open (os.path.join(cur_location, file_name), 'w') as file:
+            print('enter data to write to the file')
+            data = input()
+            file.write(data)
+            file.close
+        print('content added succesfully')
+        print('do you want to continue? (yes/no)')
+        option = input()
+        if option == 'yes':
+            start()
+        elif option == 'no':
+            print('Thank you. Good bue')
+    else:
+        print('such a file does not exist')
+        print('do you want to continue? (yes/no)')
+        option = input()
+        if option == 'yes':
+            start()
+        elif option == 'no':
+            print('Thank you. Good bue')
     pass
 
-
-def dir_options():
+def options():
         print('''
-        press 1 to rename the directory
-        press 2 to print number of files in the directory
-        press 3 to print number of directories in the directory
-        press 4 to list content of the directory
-        press 5 to add file to the directory
-        press 6 to add new directory to the directory
-        press 7 to change directory
-        ''')
-        option = int(input())
-        if option == 1:
+        press a to rename the directory
+        press b to print number of files in the directory
+        press c to print number of directories in the directory
+        press d to list content of the directory
+        press e to add file to the directory
+        press f to add new directory to the directory
+        press g to change directory
+        press h to delete file
+        press i to rename file
+        press j to add content to file
+        prees k to rewrite content of the file
+        press l to return to the parent directory''')
+        option = input()
+        if option == 'a':
             rename_directory()
-        elif option == 2:
+        elif option == 'b':
             print_files_number()
-        elif option == 3:
+        elif option == 'c':
             print_dirs_number()
-        elif option == 4:
+        elif option == 'd':
             list_content()
-        elif option == 5:
+        elif option == 'e':
             add_file()
-        elif option == 6:
+        elif option == 'f':
             add_dir()
-        elif option == 7:
+        elif option == 'g':
             change_directory()
-
-def file_options():
-        print('''
-        press 1 to delete file
-        press 2 to rename file
-        press 3 to add content to file
-        prees 4 to rewrite content of the file
-        press 5 to return to the parent directory''')
-        option = int(input())
-        if option == 1:
+        elif option == 'h':
             delete_file()
-        elif option == 2:
+        elif option == 'i':
             rename_file()
-        elif option == 3:
+        elif option == 'j':
             add_content()
-        elif option == 4:
+        elif option == 'k':
             rewrite_content()
-        elif option == 5:
+        elif option == 'l':
             return_to_parent_dir()
 
-
 def start():
-    if os.path.isdir(cur_location):
-        dir_options()
-    elif os.path.isfile(cur_location):
-        file_options()
+    global cur_location
+    print('you are now in the', cur_location, 'directory')
+    options()
 
 start()
