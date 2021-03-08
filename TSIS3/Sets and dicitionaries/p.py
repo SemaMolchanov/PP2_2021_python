d6c1377b4323e34a96f9741e21e541d9ccf6e9cd
SemaMolchanov <sema.molchanov@gmail.com>
2021-03-08 15:32:26 +0600

    TSIS3 sets and dicts p.py modified

--- TSIS3/Sets and dicitionaries/p.py
+++ TSIS3/Sets and dicitionaries/p.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 lines = list()
 while True:
     line = input()
@@ -8,7 +10,10 @@ while True:
     
 txt = ' '.join(lines)
 words = txt.split()
-pairs = set()
+
+counter = Counter(sorted(words))
+print(*sorted(counter.keys(), key=counter.get, reverse = True),sep='\n')
+'''pairs = set()
 pair = tuple()
 word, cnt = str(), str()
 for i in words:
@@ -18,7 +23,7 @@ sorted_pairs = sorted(pairs)
 reversed_pairs = reversed(sorted_pairs)
 
 for j in reversed_pairs:
-    print(j[1])
+    print(j[1])'''
 
     
 
