aaf21feba8865079ffe1386eff8a1f43b8578ed6
SemaMolchanov <sema.molchanov@gmail.com>
2021-03-12 23:25:53 +0600

    TSIS3 p.py changed

--- TSIS3/Sets and dicitionaries/p.py
+++ TSIS3/Sets and dicitionaries/p.py
@@ -1,18 +1,41 @@
-from collections import Counter
+#from collections import Counter
 
-lines = list()
+'''lines = list()
 while True:
     line = input()
-    if line:
+    if lines:
         lines.append(line)
     else:
-        break
-    
-txt = ' '.join(lines)
+        break'''
+
+'''txt = ' '.join(lines)
 words = txt.split()
 
 counter = Counter(sorted(words))
 print(*sorted(counter.keys(), key=counter.get, reverse = True),sep='\n')
+'''
+
+words = []
+
+while True:
+    line = input().split()
+    if not line:
+        break
+    for word in line:
+        words.append(word)
+
+occurence = dict()
+
+for word in words:
+    if word in occurence:
+        occurence[word] +=1
+    else :
+        occurence[word] = 1
+    
+occurence = sorted(([-value, key] for key, value in occurence.items()))
+
+for key, value in occurence:
+    print(value)
 
 
     
