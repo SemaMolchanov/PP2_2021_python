f71821290d9cef6e66ceb4701c5e49fb858e8bf1
SemaMolchanov <sema.molchanov@gmail.com>
2021-04-05 17:14:23 +0600

    TSIS7 modified

--- TSIS7/hw.py
+++ TSIS7/hw.py
@@ -1,6 +1,7 @@
 import pygame 
 import math
 
+
 pygame.init()
 
 BLACK = [0, 0, 0]
@@ -8,25 +9,36 @@ WHITE = [255, 255, 255]
 RED = [0, 0, 255]
 BLUE = [255, 0, 0]
 
-screen = pygame.display.set_mode([796, 480])
+screen = pygame.display.set_mode([930, 690])
 screen.fill([255, 255, 255])
+
 pygame.display.set_caption('function graph')
 
 done = False
 
-cos_points = []
-sin_points = []
+r_cos_points = []
+r_sin_points = []
+l_cos_points = []
+l_sin_points = []
 
-n = 6
-a = 192
+n = 3
+a = 240
 
-for x in range(77, 719):
-    y = int(math.cos(x/684 * n * math.pi) * a + 226)
-    cos_points.append([x, y])
+for x in range(105, 465):
+    y = int(math.cos(x/360 * n * math.pi) * a + 345)
+    r_cos_points.append([x, y])
 
-for x in range(77, 719):
-    y = int(math.sin(x/684 * n * math.pi) * a + 226)
-    sin_points.append([x, y])
+for x in range(105, 465):
+    y = int(math.sin(x/360 * n * math.pi) * a + 345)
+    r_sin_points.append([x, y])
+
+for x in range(465, 825):
+    y = int(math.cos(x/360 * n * math.pi) * a + 345)
+    l_cos_points.append([x, y])
+
+for x in range(465, 825):
+    y = int(math.sin(x/360 * n * math.pi) * a + 345)
+    l_sin_points.append([x, y])
 
 while not done:
     for event in pygame.event.get():
@@ -37,43 +49,60 @@ while not done:
 
     #frame
 
-    pygame.draw.rect(screen, BLACK, (56, 12, 684, 428), 3)
+    pygame.draw.rect(screen, BLACK, (75, 75, 780, 540), 3)
 
     #x-axis and y-axis
 
-    pygame.draw.line(screen, BLACK, [56, 226], [740, 226], 3)
-    pygame.draw.line(screen, BLACK, [398, 12], [398, 440], 3)
+    pygame.draw.line(screen, BLACK, [75, 345], [855, 345], 3)
+    pygame.draw.line(screen, BLACK, [465, 75], [465, 615], 3)
 
-    #left vertical lines
+    #horizontal lines
 
-    pygame.draw.line(screen, BLACK, [56, 178], [740, 178], 1)
-    pygame.draw.line(screen, BLACK, [56, 130], [740, 130], 1)
-    pygame.draw.line(screen, BLACK, [56, 82], [740, 82], 1)
-    pygame.draw.line(screen, BLACK, [56, 34], [740, 34], 1)
+    for y in range (105, 615, 60):
+        pygame.draw.line(screen, BLACK, [75, y], [855, y], 1)
 
-    #right vertical lines
+    #vertical lines
+    for x in range (105, 885, 120):
+        pygame.draw.line(screen, BLACK, [x, 75], [x, 615], 1)
 
-    pygame.draw.line(screen, BLACK, [56, 274], [740, 274], 1)
-    pygame.draw.line(screen, BLACK, [56, 322], [740, 322], 1)
-    pygame.draw.line(screen, BLACK, [56, 370], [740, 370], 1)
-    pygame.draw.line(screen, BLACK, [56, 418], [740, 418], 1)
+    #vertical scale
 
-    #upper horiszontal lines
+    u, d = 105, 585
+    i = 0
+    while (u < d):
+        if i % 2 == 0:
+            pygame.draw.line(screen, BLACK, [75, u], [90, u], 1)
+            pygame.draw.line(screen, BLACK, [840, u], [855, u], 1)
+        else:
+            pygame.draw.line(screen, BLACK, [75, u], [82, u], 1)
+            pygame.draw.line(screen, BLACK, [848, u], [855, u], 1)
+        i += 1
+        u += 15
 
-    pygame.draw.line(screen, BLACK, [291, 12], [291, 440], 1)
-    pygame.draw.line(screen, BLACK, [184, 12], [184, 440], 1)
-    pygame.draw.line(screen, BLACK, [77, 12], [77, 440], 1)
+    #horizontal scale
 
-    #lower horizontal lines
+    l, r = 105, 835
+    j = 0
+    while (l < r):
+        if j % 2 == 0:
+            pygame.draw.line(screen, BLACK, [l, 75], [l, 90], 1)
+            pygame.draw.line(screen, BLACK, [l, 600], [l, 615], 1)
+        elif j % 4 == 0:
+            pygame.draw.line(screen, BLACK, [l, 75], [l, 97], 1)
+            pygame.draw.line(screen, BLACK, [l, 593], [l, 615], 1)
+        else:
+            pygame.draw.line(screen, BLACK, [l, 608], [l, 615], 1)
+            pygame.draw.line(screen, BLACK, [l, 75], [l, 82], 1)
+        j += 1
+        l += 15
 
-    pygame.draw.line(screen, BLACK, [505, 12], [505, 440], 1)
-    pygame.draw.line(screen, BLACK, [612, 12], [612, 440], 1)
-    pygame.draw.line(screen, BLACK, [719, 12], [719, 440], 1)
-
+    
     #sine and cosine lines
 
-    pygame.draw.lines(screen, BLUE, False, cos_points, 4)
-    pygame.draw.lines(screen, RED, False, sin_points, 4)
+    pygame.draw.lines(screen, BLUE, False, r_cos_points, 2)
+    pygame.draw.lines(screen, RED, False, r_sin_points, 2)
+    pygame.draw.lines(screen, BLUE, False, l_cos_points, 2)
+    pygame.draw.lines(screen, RED, False, l_sin_points, 2)
 
     pygame.display.flip()
 
