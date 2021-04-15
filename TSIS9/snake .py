c7812c1b8a134ae9d73cb3f44ae3b44d218f902a
SemaMolchanov <sema.molchanov@gmail.com>
2021-04-15 14:52:00 +0600

    TSIS9 modified

--- TSIS9/snake .py
+++ TSIS9/snake .py
@@ -25,7 +25,7 @@ i = 1
 
 clock = pygame.time.Clock()
 
-class Snake:
+class Snake1:
 
     '''у змейки есть:
     -блоки (круглешки или квадратики) а именно:
@@ -53,10 +53,66 @@ class Snake:
         pygame.draw.circle(screen, WHITE, (self.blocks[0][0], self.blocks[0][1]), self.radius)
         for block in self.blocks[1:]:
             if i % 2 == 0:
-                pygame.draw.circle(screen, BLUE, block, self.radius)
+                 pygame.draw.circle(screen, YELLOW, block, self.radius)
             elif i % 2 != 0:
-                pygame.draw.circle(screen, YELLOW, block, self.radius)
+                pygame.draw.circle(screen, BLUE, block, self.radius)
             i += 1
+        
+
+    def add_block(self):
+        self.size += 1
+        self.blocks.insert(0, [self.blocks[0][0] + self.displacementX, self.blocks[0][1] + self.displacementY])
+        self.is_growing = False
+
+    def move(self):
+        global THE_END
+
+        if self.is_growing:
+            self.add_block()
+
+        for j in range(self.size - 1, 0, -1):
+            self.blocks[j][0] = self.blocks[j - 1][0]
+            self.blocks[j][1] = self.blocks[j - 1][1]
+
+        self.blocks[0][0] += self.displacementX
+        self.blocks[0][1] += self.displacementY
+
+        if self.blocks[0] in self.blocks[1:]:
+            THE_END = True
+
+class Snake2:
+
+    '''у змейки есть:
+    -блоки (круглешки или квадратики) а именно:
+        -голова
+        -тело (хвост)
+    -размер (кол-во блоков включая голову)
+    -направление движения
+    -состояние (растет или нет)
+    -возможность перемещения по вертикали
+    -возможность перемещения по горизонтали'''
+
+    def __init__(self):
+        self.blocks = [[700, 500]]
+        self.size = 1
+        self.displacementX = -displacement
+        self.displacementY = 0
+        self.is_growing = False
+        self.radius = radius
+        self.direction = 'left'
+
+    #рисуется полосатое тело и отдельным цветом (белым) голова
+
+    def draw(self):
+        global i
+        pygame.draw.circle(screen, WHITE, (self.blocks[0][0], self.blocks[0][1]), self.radius)
+        for block in self.blocks[1:]:
+            if i % 2 == 0:
+                 pygame.draw.circle(screen, YELLOW, block, self.radius)
+            elif i % 2 != 0:
+                pygame.draw.circle(screen, RED, block, self.radius)
+            i += 1
+        
 
     def add_block(self):
         self.size += 1
@@ -80,7 +136,8 @@ class Snake:
             THE_END = True
 
 
-snake1 = Snake()
+snake1 = Snake1()
+snake2 = Snake2()
 
 while not THE_END:
 
@@ -108,10 +165,31 @@ while not THE_END:
                 snake1.displacementX = displacement
                 snake1.displacementY = 0
                 snake1.direction = 'right'
-            elif event.key == pygame.K_SPACE:
+            elif event.key == pygame.K_s and snake2.direction != 'up':
+                snake2.displacementX = 0
+                snake2.displacementY = displacement
+                snake2.direction = 'down'
+            elif event.key == pygame.K_w and snake2.direction != 'down':
+                snake2.displacementX = 0
+                snake2.displacementY = -displacement
+                snake2.direction = 'up'
+            elif event.key == pygame.K_a and snake2.direction != 'right':
+                snake2.displacementX = -displacement
+                snake2.displacementY = 0
+                snake2.direction = 'left'
+            elif event.key == pygame.K_d and snake2.direction != 'left':
+                snake2.displacementX = displacement
+                snake2.displacementY = 0
+                snake2.direction = 'right'
+            elif event.key == pygame.K_1:
                 snake1.is_growing = True
+            elif event.key == pygame.K_2:
+                snake2.is_growing = True
     
-    snake1.move()
+    #snake1.move()
+    #screen.fill(BLACK)
+    snake2.move()
     screen.fill(BLACK)
-    snake1.draw()
+    #snake1.draw()
+    snake2.draw()
     pygame.display.flip()
