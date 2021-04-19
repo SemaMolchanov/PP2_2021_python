fb9993cf504b9ca5a4280496847c63ff3333c23d
SemaMolchanov <sema.molchanov@gmail.com>
2021-04-19 11:22:02 +0600

    TSIS9 modified

--- TSIS9/paint.py
+++ TSIS9/paint.py
@@ -7,19 +7,44 @@ import pygame, random
 # Ax + By + C = 0
 # (x - x1) / (x2 - x1) = (y - y1) / (y2 - y1)
 
-#colours
 BLACK = [0, 0, 0]
-WHITE = [255, 255, 255]
 GRAY = [192, 192, 192]
-BROWN = [102, 51, 0]
+BROWN = [153, 76, 0]
+WHITE = [255, 255, 255]
 RED = [255, 0, 0]
-ORANGE = [255, 153, 51]
+ORANGE = [255, 127, 0]
 YELLOW = [255, 255, 0]
 GREEN = [0, 255, 0]
-LIGHT_BLUE = [0, 255, 255]
+LIGHTBLUE = [0, 255, 255]
 BLUE = [0, 0, 255]
 PURPLE = [255, 0, 255]
 PINK = [255, 0, 127]
+
+class Button():
+    def __init__(self, color, x, y, width, height, text=''):
+        self.color = color
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.text = text
+
+    def draw(self, screen, outline = None):
+        if outline:
+            pygame.draw.rect(screen, outline, (self.x-2, self.y-2, self.width+4, self.height+4), 0)
+            
+        pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height), 0)
+        
+        if self.text != '':
+            font = pygame.font.SysFont('Calibri', 20)
+            text = font.render(self.text, True, BLACK)
+            screen.blit(text, (self.x + (self.width/2 - text.get_width()/2), self.y + (self.height/2 - text.get_height()/2)))
+
+    def isOver(self, pos):
+        if pos[0] > self.x and pos[0] < self.x + self.width:
+            if pos[1] > self.y and pos[1] < self.y + self.height:
+                return True 
+        return False
 
 
 
@@ -52,38 +77,12 @@ def drawLine(screen, start, end, width, color):
             x = (-C - B * y) / A
             pygame.draw.circle(screen, color, (x, y), width)
 
-def erase(screen, start, end, width, color = WHITE):
-    x1 = start[0]
-    y1 = start[1]
-    x2 = end[0]
-    y2 = end[1]
-
-    dx = abs(x1 - x2)
-    dy = abs(y1 - y2)
-
-    A = y2 - y1
-    B = x1 - x2
-    C = x2 * y1 - x1 * y2
-
-    if dx > dy:
-        if x1 > x2:
-            x1, x2 = x2, x1
-            y1, y2 = y2, y1
-
-        for x in range(x1, x2):
-            y = (-C - A * x) / B
-            pygame.draw.circle(screen, color, (x, y), width)
-    else:
-        if y1 > y2:
-            x1, x2 = x2, x1
-            y1, y2 = y2, y1
-        for y in range(y1, y2):
-            x = (-C - B * y) / A
-            pygame.draw.circle(screen, color, (x, y), width)
-
 def main():
-    screen = pygame.display.set_mode((800, 600))
+    screen = pygame.display.set_mode((800, 700))
     screen.fill(WHITE)
+
+    pygame.draw.line(screen, BLACK, [0, 100], [800, 100], 2)
+
     mode = 'random'
     draw_on = False
     last_pos = (0, 0)
@@ -96,6 +95,31 @@ def main():
         'green': (0, 255, 0)
     }
 
+    black_button = Button(BLACK, 100, 5, 80, 40)
+    brown_button = Button(BROWN, 100, 50, 80, 40)
+    gray_button = Button(GRAY, 200, 5, 80, 40)
+    red_button = Button(RED, 200, 50, 80, 40)
+    orange_button = Button(ORANGE, 300, 5, 80, 40)
+    yellow_button = Button(YELLOW, 300, 50, 80, 40)
+    green_button = Button(GREEN, 400, 5, 80, 40)
+    lightblue_button = Button(LIGHTBLUE, 400, 50, 80, 40)
+    blue_button = Button(BLUE, 500, 5, 80, 40)
+    purple_button = Button(PURPLE, 500, 50, 80, 40)
+    pink_button = Button(PINK, 600, 5, 80, 40)
+    eraser_button = Button(WHITE, 600, 50, 80, 40)
+
+    black_button.draw(screen, BLACK)
+    gray_button.draw(screen, BLACK)
+    brown_button.draw(screen, BLACK)
+    red_button.draw(screen, BLACK)
+    orange_button.draw(screen, BLACK)
+    yellow_button.draw(screen, BLACK)
+    green_button.draw(screen, BLACK)
+    lightblue_button.draw(screen, BLACK)
+    blue_button.draw(screen, BLACK)
+    purple_button.draw(screen, BLACK)
+    pink_button.draw(screen, BLACK)
+    eraser_button.draw(screen, BLACK)
 
     while True:
 
@@ -104,6 +128,7 @@ def main():
         ctrl_held = pressed[pygame.K_LCTRL] or pressed[pygame.K_RCTRL]
 
         for event in pygame.event.get():
+            pos = pygame.mouse.get_pos()
             if event.type == pygame.QUIT:
                 return
             if event.type == pygame.KEYDOWN:
